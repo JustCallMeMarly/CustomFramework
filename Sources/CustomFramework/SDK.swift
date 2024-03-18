64fc4a2a42153998638da2ff79a03d38fd4ed45f
marly <marchele8@gmail.com>
2024-03-17 19:31:45 -0600

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Marchele Hill on 3/17/24.
+//
+
+import Foundation
+
+
+struct SDK {
+    static func doSomeWork() {
+        print("Doing some work....")
+    }
+}
